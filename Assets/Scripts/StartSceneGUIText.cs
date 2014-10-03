edb2b70fa90a0bb523fc47e15e05769e8435a07b
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2014-10-03 18:37:27 +0200

    Start scene with GUI Text and implemented functionality of loading next scene when user press space key after text shows

---
+++ Assets/Scripts/StartSceneGUIText.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Scripts/GUI/Machine Text", 0)]
+
+public class StartSceneGUIText : MonoBehaviour {
+
+    [Range(0.0001f, 1.0f)]
+    public float Rate = 1.0f;
+
+    private string text = "";
+    private float timer = 0.0f;
+    private int i = 0;
+    private bool showText = true;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    text = "Tu bedzie mega fajny tekst wymyslony przez majstra\n";
+        text += "A tu bedzie druga linijka\n";
+        text += "\nNacisnij spacje";
+        if(this.guiText == null)
+        {
+            Debug.LogError("ERROR!!! This script should be attached to GUIText!!!");
+            Debug.Break();
+        }
+        else
+        {
+            this.guiText.text = "";
+        }
+        timer = Rate + 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (showText)
+        {
+            if (timer > Rate)
+            {
+                this.guiText.text += text[i];
+                i++;
+                timer = 0.0f;
+            }
+            else
+            {
+                timer += Time.deltaTime;
+            }
+
+            if (i == text.Length) showText = false;
+        }
+        else
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Application.LoadLevel(Application.loadedLevel + 1);
+            }
+        }
+	}
+}
