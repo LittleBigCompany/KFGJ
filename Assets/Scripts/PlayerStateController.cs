406fa6c9a257601b069afdbfbf127c617dd0e36c
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2014-10-03 20:32:49 +0200

    * Timer is done (we can also hide timer turning variable "ShowTimer" in Timer object off) * Player state manager (tells us when player is alive and can run or something)

---
+++ Assets/Scripts/PlayerStateController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum State { Alive = 1, Dead };
+
+public class PlayerStateController : MonoBehaviour {
+
+    public State CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+    }
+
+    public GUIText DeathText;
+    public string EndOfTimeString = "You're too slow!!";
+    public string WallHitString = "You're retarted!!";
+    public string DefaultDeathString = "You are dead!";
+
+    private State currentState = State.Alive;
+
+	// Use this for initialization
+	void Start () 
+    {
+        DeathText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+    public void Die(string cause = "Deafult")
+    {
+        //Some nice explosion particles will be invoked here
+        currentState = State.Dead;
+        switch(cause)
+        {
+            case "EndOfTime":
+                DeathText.text = EndOfTimeString;
+                break;
+            case "WallHit":
+                DeathText.text = WallHitString;
+                break;
+            default:
+                DeathText.text = DefaultDeathString;
+                break;
+        }
+        DeathText.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Scripts/GUI/Timer")]
+
+public class Timer : MonoBehaviour {
+
+    [Tooltip("This is time in minutes")]
+    public float TimeLeft = 10.0f;
+    public PlayerStateController StateController;
+    public GUIText GUITimer;
+    public bool ShowTimer = true;
+
+    private float time = 0.0f;
+    private float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        time = TimeLeft * 60.0f;
+        GUITimer.enabled = ShowTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timer += Time.deltaTime;
+        if(timer > TimeLeft)
+        {
+            StateController.Die("EndOfTime");
+        }
+        if(ShowTimer)
+        {
+            GUITimer.text = ConvertSecondsToMinutes(60 * TimeLeft - timer);
+        }
+	}
+
+    public static string ConvertSecondsToMinutes(float sec)
+    {
+        int seconds = (int)sec;
+        int minutes = (int)seconds / 60;
+        seconds = seconds - minutes * 60;
+        string time = "";
+        if (minutes < 10)
+        {
+            time += "0";
+            time += minutes.ToString();
+        }
+        else
+        {
+            time += minutes.ToString();
+        }
+
+        time += ":";
+
+        if (seconds < 10)
+        {
+            time += "0";
+            time += seconds.ToString();
+        }
+        else
+        {
+            time += seconds.ToString();
+        }
+        return time;
+    }
+}
