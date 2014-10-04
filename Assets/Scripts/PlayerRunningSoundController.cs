c5f479fbb9b0e54ca1d5ee8e220a7d78b11b578f
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2014-10-04 12:39:49 +0200

    * Running sound

---
+++ Assets/Scripts/PlayerRunningSoundController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Scripts/Player/Running controller")]
+public class PlayerRunningSoundController : MonoBehaviour {
+
+    public Rigidbody Player;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        this.GetComponent<Animator>().SetFloat("velocity", Player.velocity.magnitude);
+	}
+}
