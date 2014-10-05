b713062ea4ac201cb6ab9ad48a4a3ef1670f3101
Patryk Kochański <patryk.kochanski@hotmail.com>
2014-10-05 12:51:42 +0200

    Adding Drawings prefabs which will be added to walls prefabs :)

---
+++ Assets/Scripts/DrawingsManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Scripts/Drawings/DrawingsManager",0)]
+
+public class DrawingsManager : MonoBehaviour {
+
+    public Texture[] Textures = new Texture[6];
+
+    private int chose;
+
+	// Use this for initialization
+	void Start () {
+        chose = Random.Range(0,Textures.Length);
+        this.renderer.material.mainTexture = Textures[chose];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
